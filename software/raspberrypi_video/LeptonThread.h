465c75a9dcb693b49aadec0b854e84a9b70c3492
Shaqran Bin Saleh <53170558+shaqran39@users.noreply.github.com>
2021-12-30 23:12:43 +0600

    Add files via upload

---
+++ software/raspberrypi_video/LeptonThread.h
@@ -0,0 +1,88 @@
+#ifndef TEXTTHREAD
+#define TEXTTHREAD
+
+#include <ctime>
+#include <stdint.h>
+
+#include <QThread>
+#include <QtCore>
+#include <QPixmap>
+#include <QImage>
+#include <QUdpSocket>
+
+#define PACKET_SIZE 164
+#define PACKET_SIZE_UINT16 (PACKET_SIZE/2)
+#define PACKETS_PER_FRAME 60
+#define FRAME_SIZE_UINT16 (PACKET_SIZE_UINT16*PACKETS_PER_FRAME)
+
+class LeptonThread : public QThread
+{
+  Q_OBJECT;
+
+public:
+  QPoint lastPoint;
+  float extractedTemp;
+
+  // ISHRAQ __ CUSTOM 2D array that is constantly updated with values from frame
+  // Lepton 3.5 is used, so using 120 rows, and 160 columns for each frame in buffer
+  float matrix[120][160];
+
+  LeptonThread();
+  ~LeptonThread();
+
+  float getTempFromXY(int x, int y);
+  float getTempFromArea(int x1, int y1, int x2, int y2);
+
+  void startTimeout();
+
+  void setLogLevel(uint16_t);
+  void useColormap(int);
+  void useLepton(int);
+  void useSpiSpeedMhz(unsigned int);
+  void setAutomaticScalingRange();
+  void useRangeMinValue(uint16_t);
+  void useRangeMaxValue(uint16_t);
+  void run();
+
+public slots:
+  void performFFC();
+  void timeout();
+  void processPendingDatagrams();
+
+
+signals:
+  void updateText(QString);
+  void updateImage(QImage);
+  void readyRead();
+
+private:
+  int x1;
+  int y1;
+  int x2;
+  int y2;
+  bool ok;
+  QWidget * parent;
+  void log_message(uint16_t, std::string);
+  uint16_t loglevel;
+  int typeColormap;
+  const int *selectedColormap;
+  int selectedColormapSize;
+  int typeLepton;
+  unsigned int spiSpeed;
+  bool autoRangeMin;
+  bool autoRangeMax;
+  uint16_t rangeMin;
+  uint16_t rangeMax;
+  int myImageWidth;
+  int myImageHeight;
+  QImage myImage;
+
+  uint8_t result[PACKET_SIZE*PACKETS_PER_FRAME];
+  uint8_t shelf[4][PACKET_SIZE*PACKETS_PER_FRAME];
+  uint16_t *frameBuffer;
+
+  QUdpSocket * udpSocket;
+
+};
+
+#endif
